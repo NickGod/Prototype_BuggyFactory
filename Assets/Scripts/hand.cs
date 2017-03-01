2ad1128471802c4c3f1038388e94879ac3f7baa5
JoeETC <duduyi2015@gmail.com>
2017-03-01 17:37:02 -0500

    add basic mechanism into the game

---
+++ Assets/Scripts/hand.cs
@@ -0,0 +1,210 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class hand : MonoBehaviour {
+    //public Transform _rightHand;
+    //static Transform _rightIndex;
+    // accept three elements, first is inventory slot
+    // second is demonstrate spot
+    // third is orbitting slot
+
+
+    static float _inventoryTimer = 0;
+    static float _inventoryTime = 0.2f;
+    static bool _isInventory = false;
+
+    public List<Transform> trfList = new List<Transform>();
+
+    Transform _grabbedParent;
+    Transform _grabbed;
+
+    static Transform _singleTestTrf = null;
+    static Transform[] _flowTestTrfs = new Transform[3];  
+
+    
+    //aiming and grabbing/pointing
+    bool _isFist = false;
+    bool _isGrabbing = false;
+    
+    //resizing
+    //static float _originDistance = 0.0f;
+    //static bool _onResizing = false;
+
+    public bool isRightHand;
+    // Update is called once per frame
+
+    //right index finger
+    //static bool isIndexFound = false;
+    //LineRenderer lineRender;
+
+    void Start() {
+        //lineRender = GetComponent<LineRenderer>();
+    }
+
+    void Update() {
+        if (!isRightHand) {
+            ////for left hand
+
+
+            //transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+            //transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+
+
+            //calling inventory based on left hand index
+            if (IsInventoryUp()) {
+                _inventoryTimer += Time.deltaTime;
+            } else {
+                _inventoryTimer = 0.0f;
+                if (_isInventory) {
+                    //TODO: inventory off animation and related mehanics here
+                    transform.GetChild(0).gameObject.SetActive(false);
+                }
+                _isInventory = false;
+            }
+            if (_inventoryTimer >= _inventoryTime && !_isInventory) {
+                // this function only call once for each index up
+                // there is up time for inventory to appear
+                //TODO: inventory on animation and related mechanics here
+                _isInventory = true;
+                transform.GetChild(0).gameObject.SetActive(true);
+            }
+        } else {
+            //for right hand
+            //grabbing
+            _isGrabbing = false;
+            if (IsFist() && !_isFist) {
+                _isFist = true;
+                _isGrabbing = true;
+            }
+            if (_isGrabbing) {
+                if (_grabbed == null) {
+                    int count = trfList.Count;
+                    for (int i = count - 1; i >= 0; i--) {
+                        Transform trf = trfList[i];
+                        if (trf == null || !trf.gameObject.activeSelf) {
+                            trfList.Remove(trf);
+                        }
+                    }
+                    Transform target = GetClosest();
+                    if (target) {
+                        _grabbed = target.GetComponent<module>().OnGrab();
+                        if (!trfList.Contains(_grabbed)) {
+                            trfList.Add(_grabbed);
+                        }
+                        if (_grabbed) {
+                            _grabbedParent = null;
+                            _grabbed.parent = transform;
+                        }
+                    }
+                }
+            }
+
+            //grabbing release
+            if (!IsFist()) {
+                if (_grabbed) {
+                    _grabbed.parent = _grabbedParent;
+                    LoseControl();
+                }
+                _isFist = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == Tags.Grabbable && !trfList.Contains(other.transform)) {
+            trfList.Add(other.transform);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == Tags.Grabbable) {
+            trfList.Remove(other.transform);
+        }
+    }
+
+    Transform GetClosest() {
+        Transform select = null;
+        float _minDistance = float.MaxValue;
+        foreach (var trf in trfList) {
+            float dis = Vector3.Distance(trf.position, transform.position);
+            if (dis < _minDistance) {
+                _minDistance = dis;
+                select = trf;
+            }
+        }
+        return select;
+    }
+
+    bool IsInventoryUp() {
+        if (!isRightHand) {
+            return !(OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger) || OVRInput.Get(OVRInput.NearTouch.PrimaryIndexTrigger) ||
+                OVRInput.Get(OVRInput.Touch.PrimaryThumbRest) || OVRInput.Get(OVRInput.Touch.PrimaryThumbstick));
+        } else {
+            return false;
+        }
+    }
+
+    bool IsFist() {
+        OVRInput.Button indexFinger;
+        OVRInput.Button middleFinger;
+
+        bool thumb;
+        if (isRightHand) {
+            indexFinger = OVRInput.Button.SecondaryIndexTrigger;
+            middleFinger = OVRInput.Button.SecondaryHandTrigger;
+            thumb = OVRInput.Get(OVRInput.Touch.SecondaryThumbRest) || OVRInput.Get(OVRInput.Touch.One) || OVRInput.Get(OVRInput.Touch.Two);
+        } else {
+            indexFinger = OVRInput.Button.PrimaryIndexTrigger;
+            middleFinger = OVRInput.Button.PrimaryHandTrigger;
+            thumb = OVRInput.Get(OVRInput.Touch.PrimaryThumbRest) || OVRInput.Get(OVRInput.Touch.Three) || OVRInput.Get(OVRInput.Touch.Four);
+        }
+        bool index = OVRInput.Get(indexFinger);
+        bool middle = OVRInput.Get(middleFinger);
+
+        return index && middle && thumb;
+    }
+
+    public Transform GetGrabbedParent() {
+        return _grabbedParent;
+    }
+
+    public void LoseControl() {
+        if (_grabbed) {
+            Transform _result = _grabbed.GetComponent<module>().OnRelease(this);
+            //TODO: _singleTestTrf = result or  _flowTestTrfs[i] = result
+            //TODO: destroy the stuff existing previously
+            module _resultModule = _result.GetComponent<module>();
+            if (_resultModule._mySpot == module.SpotType.SingleTest) {
+                Destroy(_singleTestTrf);
+                _singleTestTrf = _result;
+
+            } else if (_resultModule._mySpot == module.SpotType.FlowTest) {
+                switch(_resultModule._myType) {
+                    case module.ModuleType.type1:
+                        Destroy(_flowTestTrfs[0]);
+                        _flowTestTrfs[0] = _result;
+                        break;
+                    case module.ModuleType.type2:
+                        Destroy(_flowTestTrfs[1]);
+                        _flowTestTrfs[1] = _result;
+                        break;
+                    case module.ModuleType.type3:
+                        Destroy(_flowTestTrfs[2]);
+                        _flowTestTrfs[2] = _result;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            _grabbed = null;
+            _grabbedParent = null;
+        }
+    }
+
+    public void GetOutOfList(Transform trf) {
+        if (trfList.Contains(trf)) {
+            trfList.Remove(trf);
+        }
+    }
+}
---
+++ Assets/Scripts/module.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class module : MonoBehaviour {
+    public enum ModuleType {
+        type1,
+        type2,
+        type3
+    };
+    public enum SpotType {
+        Module,
+        SingleTest,
+        FlowTest
+    }
+    public ModuleType _myType;
+    public SpotType _mySpot = SpotType.Module;
+    public Transform singleTest;
+    public Transform[] flowTest = new Transform[3];
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Transform OnGrab() {
+        Transform me_clone = Instantiate(gameObject).transform;
+        me_clone.parent = null;
+        me_clone.position = transform.position;
+        me_clone.rotation = transform.rotation;
+        Transform _parent = transform;
+        //multiply parents' scale
+        while (_parent.parent != null) {
+            me_clone.localScale *= _parent.parent.localScale.x;
+            _parent = _parent.parent;
+        }
+        //initialize attribute value
+        me_clone.GetComponent<module>()._myType = _myType;
+        return me_clone;
+    }
+
+    public Transform OnRelease(hand _playerHand) {
+        transform.localScale = Vector3.one;
+        if (transform.position.x < -0.2f) {
+            _playerHand.GetOutOfList(gameObject.transform);
+            Debug.Log("Goes to single test.");
+            transform.position = singleTest.position;
+            _mySpot = SpotType.SingleTest;
+            return transform;
+        } else {
+            _playerHand.GetOutOfList(gameObject.transform);
+            Debug.Log("Goes to flow test.");
+            switch(_myType) {
+                case ModuleType.type1:
+                    transform.position = flowTest[0].position;
+                    break;
+                case ModuleType.type2:
+                    transform.position = flowTest[1].position;
+                    break;
+                case ModuleType.type3:
+                    transform.position = flowTest[2].position;
+                    break;
+                default:
+                    Debug.Log("Weird, shoundn't be default");
+                    break;
+            }
+            _mySpot = SpotType.FlowTest;
+            return transform;
+        }
+    }
+
+    public void move_towards(Vector3 taret_pos) {
+        transform.position += 0.05f * (taret_pos - transform.position);
+    }
+}
